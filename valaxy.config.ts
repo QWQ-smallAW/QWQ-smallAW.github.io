f8bce0f5a455f79c38f314f436cd68be3b1d0c9b
QWQ-smallAW <smallaw0731@1163.com>
2026-04-04 14:07:31 +0800

    first upload

---
+++ valaxy.config.ts
@@ -0,0 +1,25 @@
+import type { UserThemeConfig } from 'valaxy-theme-yun'
+import { defineValaxyConfig } from 'valaxy'
+
+// add icons what you will need
+const safelist = [
+  'i-ri-home-line',
+]
+
+/**
+ * User Config
+ */
+export default defineValaxyConfig<UserThemeConfig>({
+  // site config see site.config.ts
+
+  theme: 'yun',
+
+  themeConfig: {
+    banner: {
+      enable: true,
+      title: '渊林的小站',
+    },
+  },
+
+  unocss: { safelist },
+})
